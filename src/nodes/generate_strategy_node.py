67eba02525adb7395ffcb818ecef3edfb1876eea
jongmai <whdghksdl906@gmail.com>
2025-12-03 11:39:08 +0900

    refactor: generate_strategy_node에 retrieve_node 기능 통합

--- src/nodes/generate_strategy_node.py
+++ src/nodes/generate_strategy_node.py
@@ -1,17 +1,23 @@
 from src.models.state import CivilComplaintState
-import transformers
-import torch
-from langchain_community.llms import HuggingFacePipeline
+from src.tools.civil_rag_tool import get_civil_retriever
 
+retriever = get_civil_retriever()
 
 def generate_strategy_node(state: CivilComplaintState) -> CivilComplaintState:
     """
-    RAG 결과와 질문을 참고해서 답변 전략을 생성하는 노드.
+    질문을 참고해서 RAG을 통해 관련 문서를 가져오고,
+    답변 전략을 생성하는 노드.
     """
     print("---GENERATE STRATEGY NODE---")
     refined_question = state.get("refined_question", "")
-    rag_context = state.get("rag_context", "")
+
+    docs = retriever.invoke(refined_question)
+    rag_context = "\n\n".join(
+        f"[{i+1}] sorce: {doc.metadata.get('source_file', 'unknown')}\n{doc.page_content}"
+        for i , doc in enumerate(docs)
+        )
 
     # Mock strategy generation
-    strategy = f"Strategy for: {refined_question}"
-    return {"strategy": strategy}
+    # rag_context를 활용하여 실제로 전략을 생성하는 로직이 들어가야 함
+    strategy = f"Strategy for: {refined_question} using {len(docs)} documents."
+    return {"strategy": strategy, "rag_context": rag_context}
